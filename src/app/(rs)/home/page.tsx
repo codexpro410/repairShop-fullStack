a364c92f5d3cd96af7493c7832eac374c953481f
codexpro410 <codexpro410@gmail.com>
2025-02-15 18:14:00 +0200

    fix:home page style

--- src/app/(rs)/home/page.tsx
+++ src/app/(rs)/home/page.tsx
@@ -97,7 +97,7 @@ export default function Home() {
       </section>
 
       {/* Contact/Location Section */}
-      <section className="w-full py-16  text-white">
+      <section className="w-full py-16">
         <div className="container mx-auto">
           <h2 className="text-3xl font-bold text-center mb-10">Visit Us</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
